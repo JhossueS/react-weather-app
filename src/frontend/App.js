b7cd1ffdaa0376c4b2dac58dc3ca744e3962e511
jhossue sanchez <jhossue.d.sanchez@gmail.com>
2021-01-08 00:31:00 -0500

    creacion de footer

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -27,6 +27,29 @@ const App = () => {
         <Highlights />
         <Highlights />
       </div>
+      <div className='App_about__container'>
+
+        <div className='App_about__container-contact'>
+          <span className='about-title'>
+            Contact:
+            {'  '}
+          </span>
+          <span> jhossue.d.sacnhez@gmail.com</span>
+        </div>
+        <div className='App_about__container-link'>
+          <span className='about-title'>
+            design and challenge at:
+            {'  '}
+          </span>
+          <a
+            href='https://devchallenges.io/challenges/mM1UIenRhK808W8qmLWv'
+            target='_blank'
+            className='link'
+          >
+            devchallenges.io
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
